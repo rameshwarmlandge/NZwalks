d6d2dbc2fc9a9baab3dff5855039e05658fb6e0f
Rameshwar Landge <rameshwar.landge@sagitec.com>
2022-07-03 12:35:46 +0530

    Initial set up api application

---
+++ NZWalks.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NZWalks.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
