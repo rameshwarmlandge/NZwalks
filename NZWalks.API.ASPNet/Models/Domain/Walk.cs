8b378192dda4f318596d5cfbe7fc84851de012a5
Rameshwar Landge <rameshwar.landge@sagitec.com>
2022-07-05 16:35:24 +0530

    still database migration and auto create table and database.

---
+++ NZWalks.API.ASPNet/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.ASPNet.Models.Domain
+{
+    public class Walk
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public double Lenght { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficulyId { get; set; }
+
+        // Navigation property
+
+        public Region region { get; set; }
+        public WalkDifficulty WalkDifficulty  { get; set; }
+
+    }
+}
